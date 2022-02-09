6826820009b49aca74e5cb2dc49579865da32b68
Vinicius Parente <vd.parente@hotmail.com>
2022-02-09 18:09:09 -0300

    Controller Base API

---
+++ DevReviews.API/Controllers/ProductReviewsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevReviews.API.Controllers
+{
+    [ApiController]
+    [Route("api/products/{productId}/productreviews")]
+    public class ProductReviewsController : ControllerBase
+    {
+        // GET api/products/1/productreviews/5
+        [HttpGet("{id}")]
+        public IActionResult GetById(int productId, int id)
+        {
+            // Se não existir com o id especificado, retornar NotFound()
+
+            return Ok();
+        }
+
+        // POST api/products/1/productreviews
+        [HttpPost]
+        public IActionResult Post(int productId, object model)
+        {
+            // Se estiver com dados inválidos, retornar BadRequest()
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevReviews.API/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevReviews.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+        }
+
+        // GET para api/products
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        // GET para api/products/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // Se não achar, retornar NotFound()
+
+            return Ok();
+        }
+
+        // POST para api/products
+        [HttpPost]
+        public IActionResult Post(object model)
+        {
+            // Add o objeto
+            // Se tiver erros de validação, retornar BadRequest()
+            return CreatedAtAction(nameof(GetById), new { id = 1 /* id do model*/ }, model);
+        }
+
+        // PUT para api/products/{id}
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, object model)
+        {
+            // Se tiver erros de validação, retornar BadRequest()
+            // Se não existir produto com o id especificado, retornar NotFound()
+
+            return NoContent();
+        }
+    }
+
+}
+
+
+
